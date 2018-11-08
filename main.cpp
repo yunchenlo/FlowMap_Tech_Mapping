e5da013b20d13ef11368c0997fb278a0616f57ed
Jason Lo <tarzen0509@gmail.com>
2018-11-08 19:45:59 +0800

    [update] topological sort OK

--- main.cpp
+++ main.cpp
@@ -3,13 +3,21 @@
 #include <iterator>
 #include <vector>
 #include <sstream>
+#include <list> 
+#include <stack>
 
 using namespace std;
 
+// register funtions
 void read_aag(const string infile_name);
+void topologicalSort();
+void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
+void printArray();
 
+// global variable
 int M=0, I=0, O=0;
 int *adjacent_matrix = NULL;
+list<int> *adj = NULL; 
 bool *input = NULL, *output = NULL;
 
 int main(int argc, char *argv[])
@@ -17,21 +25,65 @@ int main(int argc, char *argv[])
     if(argc != 4)
     {
 	 cout << 
-	     "usage: ./kLUT <input_file_name> <K> <output_file_name>" << endl;
+	     "usage: ./FlowMap.exe <input_file_name> <K> <output_file_name>" << endl;
 	 return 1;
     }
 
     read_aag(argv[1]);
-    for(int i = 0; i < M; i++){
+    topologicalSort();
+    
+    printArray();
+	return 0;
+}
+
+void topologicalSortUtil(int v, bool visited[], stack<int> &Stack) 
+{ 
+    // Mark the current node as visited. 
+    visited[v] = true; 
+  
+    // Recur for all the vertices adjacent to this vertex 
+    list<int>::iterator i; 
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) 
+        if (!visited[*i]) 
+            topologicalSortUtil(*i, visited, Stack); 
+  
+    // Push current vertex to stack which stores result 
+    Stack.push(v); 
+} 
+
+void topologicalSort() 
+{ 
+    stack<int> Stack; 
+  
+    // Mark all the vertices as not visited 
+    bool *visited = new bool[M]; 
+    for (int i = 0; i < M; i++) 
+        visited[i] = false; 
+  
+    // Call the recursive helper function to store Topological 
+    // Sort starting from all vertices one by one 
+    for (int i = 0; i < M; i++) 
+      if (visited[i] == false) 
+        topologicalSortUtil(i, visited, Stack); 
+  
+    // Print contents of stack 
+    while (Stack.empty() == false) 
+    { 
+        cout << Stack.top() << " "; 
+        Stack.pop(); 
+    } 
+    cout << endl;
+} 
+
+void printArray(){
+	for(int i = 0; i < M; i++){
     	for(int j = 0; j < M; j++){
     		cout << adjacent_matrix[i*M + j] << " ";
     	}
     	cout << endl;
     }
     cout << endl;
-	return 0;
 }
-
 
 void read_aag(const string infile_name)
 {
@@ -59,6 +111,7 @@ void read_aag(const string infile_name)
 		
 		// malloc adjacent matrix + input table + output table
 		adjacent_matrix = new int[M*M];
+		adj = new list<int>[M];
 		input = new bool[M];
 		output = new bool[M];
 
@@ -81,7 +134,7 @@ void read_aag(const string infile_name)
 			output[stoi(line)-1] = true;
 		}
 		// fill in adjacent matrix
-		int *temp = new int[3];
+		int *temp = new int[3]; //temp[0]:root|temp[1,2]:child
 		while(getline (myfile,line)){
 			//cout << line << '\n';
 			istringstream buf(line);
@@ -99,6 +152,8 @@ void read_aag(const string infile_name)
 			}
 			adjacent_matrix[temp[0]*M + temp[1]] = 1;
 			adjacent_matrix[temp[0]*M + temp[2]] = 1;
+			adj[temp[0]].push_back(temp[1]);
+			adj[temp[0]].push_back(temp[2]);
 		}
 
 		myfile.close();
